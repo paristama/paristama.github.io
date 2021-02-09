d46011c51af7e0d9d1bcbc6589e571fc14557c65
paristama <parisxtama@gmail.com>
2021-02-09 23:16:26 +0700

    initial commit

---
+++ script.js
@@ -0,0 +1,5 @@
+const navbarToggler = document.querySelector(".navbar-toggler");
+const showNav = () => console.log("tes");
+navbarToggler.addEventListener('click', showNav);
+
+const navWrapper = document.querySelector(".nav")
